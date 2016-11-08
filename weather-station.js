80e07e7333f1a7417add3f8a9dc2e37531e09b8f
bpthur <ben.thurecht@gmail.com>
2016-11-07 21:08:29 -0500

    Initial commit

---
+++ weather-station.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var AWS = require('aws-sdk');
+var config = require('./config');
+var producer = require('./producer');
+
+var kinesis = new AWS.Kinesis({region : config.kinesis.region});
+producer(kinesis, config.producer).run();
