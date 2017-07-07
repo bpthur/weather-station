0a45271d469c2e8d706cce848e3404aed749cf1a
bpthur <ben.thurecht@gmail.com>
2017-07-06 23:23:14 -0400

    Updated gitignore and removed kinesis test harness

--- kinesis/weather-station.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var AWS = require('aws-sdk');
-var config = require('./config');
-var producer = require('./producer');
-
-var kinesis = new AWS.Kinesis({region : config.kinesis.region});
-producer(kinesis, config.producer).run();
