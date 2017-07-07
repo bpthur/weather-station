88b4955a0f147eb3431654ce1e9be2419aa94bf5
bpthur <ben.thurecht@gmail.com>
2017-07-06 23:18:56 -0400

    Added cloudwatch upload function

---
+++ cloudwatch.py
@@ -0,0 +1,31 @@
+import boto3
+import datetime
+
+# Upload a temperature reading for a
+# particular location and sensor
+def putData( location, sensor, temperature):
+
+    client = boto3.client('cloudwatch')
+
+    client.put_metric_data(
+        Namespace='weather-station',
+        MetricData=[
+            {
+                'MetricName': 'Temperature',
+                'Dimensions': [
+                    {
+                        'Name': 'Location',
+                        'Value': location
+                    },
+                    { 
+                        'Name': 'Sensor',
+                        'Value': sensor
+                    },
+                ],
+                'Timestamp': datetime.datetime.now(),
+                'Value': temperature,
+                'Unit': 'None'
+            },
+        ]
+    )
+    return;
